6f0599c75e76336ad9265dbd799e04b5c39d64ae
Nishant Gopalakrishnan <ngopalak@fhcrc.org>
2008-12-16 22:11:12 +0000

    removed quotes from do.call
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AffyExpress@36033 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/SortMat.R
+++ R/SortMat.R
@@ -3,14 +3,14 @@ SortMat <- function(Mat, Sort) {
     if (dim(Mat)[2] == 1){
         temp<-rep(NA, dim(Mat)[1])
         Mat_temp0<-cbind(Mat, temp)
-        m <- do.call("order", as.data.frame(Mat_temp0[,Sort]))
+        m <- do.call(order, as.data.frame(Mat_temp0[,Sort]))
         result0<-Mat_temp0[m,]
         result <-data.frame(result0[,1])
         rownames(result)<-rownames(result0)
         colnames(result)<-colnames(result0)[1]
     }
     else{
-        m <- do.call("order", as.data.frame(Mat[,Sort]))
+        m <- do.call(order, as.data.frame(Mat[,Sort]))
         result<-Mat[m,]
     }
     result
